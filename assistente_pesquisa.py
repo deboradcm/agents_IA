9dc6325336a64cab6002bee6fe640287a5d72e3e
deboradcm <debora.medeiros@icomp.ufam.edu.br>
2024-11-13 16:04:29 -0400

    Adiciona inicialização do cliente MongoDB e configuração da conexão com o banco de dados 'agent_demo'

--- assistente_pesquisa.py
+++ assistente_pesquisa.py
@@ -1,17 +1,27 @@
 import os
 import pandas as pd
 from datasets import load_dataset
+from pymongo import MongoClient
 
 import os
 os.environ["OPENAI_API_KEY"] = ""
 os.environ["FIREWORKS_API_KEY"] = ""
-os.environ["MONGO_URI"] = ""
+os.environ["MONGO_URI"] = "mongodb+srv://deboramedeiros:mecanica33@cluster0.6rm44.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 FIREWORKS_API_KEY = os.environ.get("FIREWORKS_API_KEY")
 OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
 MONGO_URI = os.environ.get("MONGO_URI")
 
-data = load_dataset("MongoDB/subset_arxiv_papers_with_emebeddings")
+data = load_dataset("MongoDB/subset_arxiv_papers_with_emebeddings") # carregando um dataset do Hugging Face Datasets
 dataset_df = pd.DataFrame(data["train"])
 
 print(dataset_df.head())
+
+# Initialize MongoDB python client
+client = MongoClient(MONGO_URI, appname="devrel.content.ai_agent_firechain.python")
+
+DB_NAME = "agent_demo"
+COLLECTION_NAME = "knowledge"
+ATLAS_VECTOR_SEARCH_INDEX_NAME = "vector_index"
+collection = client[DB_NAME][COLLECTION_NAME]
+
